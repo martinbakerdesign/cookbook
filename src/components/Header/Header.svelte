b905411832dca6828b76e730a37cfa1e718ec712
Martin Baker <martinbakerdesign@gmail.com>
2022-01-17 17:05:54 +1100

    Header.svelte: Added sticky state logic & styling

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -3,13 +3,23 @@
   import Search from "./Search/HeaderSearch.svelte";
   import Controls from "./Controls/HeaderControls.svelte";
   import { location } from "svelte-spa-router";
+  import { scrollY } from "store/menu";
 
-  let state;
+  let state, stuck;
+  let offset = 0;
 
   $: state = $location === "/" ? "DEFAULT" : "BASIC";
+
+  $: (stuck = $scrollY >= 20),
+    (offset = $location === "/" ? Math.min($scrollY, 20) * -1 : 0);
 </script>
 
-<header id="header" data-state={state}>
+<header
+  id="header"
+  data-state={state}
+  class:stuck
+  style={`transform: translate3d(-50%,${offset}px,0)`}
+>
   <Title />
   <Search />
   <Controls />
@@ -24,8 +34,8 @@
   #header {
     position: fixed;
     top: s.$s4;
-    left: s.$s4;
-    right: s.$s4;
+    left: 50%;
+    width: calc(100% - 2.5rem);
     border-radius: 0.75rem;
     // width: 100%;
     background-color: c.$white;
@@ -33,10 +43,23 @@
     // grid-template-columns: $s9 1fr $s9;
     grid-template-columns: 1fr var(--main) 1fr;
     z-index: l.$header;
+    will-change: transform;
+    transform: translate3d(-50%, 0, 0);
 
+    &.stuck {
+      border-radius: 0;
+      margin: 0;
+      width: 100%;
+      padding-right: s.$s4;
+      padding-left: s.$s4;
+    }
     &[data-state="BASIC"] {
       background-color: transparent;
       pointer-events: none;
+      padding-left: 0;
+      padding-right: 0;
+      width: 100%;
+      top: 0;
     }
   }
 
