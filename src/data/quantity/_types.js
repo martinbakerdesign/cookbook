2a2d2e6466172bd9a4a837c60b60fab1717b4080
Martin Baker <martinbakerdesign@gmail.com>
2022-08-17 12:01:29 +1000

    Quantity types

---
+++ src/data/quantity/_types.js
@@ -0,0 +1,18 @@
+const quantityTypes = {
+  FLOAT: "FLOAT",
+  INTEGER: "INTEGER",
+  NUMBER: "NUMBER",
+  RANGE: "RANGE",
+  DECONSTRUCTED: "DECONSTRUCTED",
+  FRACTION: "FRACTION",
+  STRING: "STRING",
+  float: "FLOAT",
+  integer: "INTEGER",
+  number: "NUMBER",
+  range: "RANGE",
+  deconstructed: "DECONSTRUCTED",
+  fraction: "FRACTION",
+  string: "STRING",
+};
+
+export default quantityTypes;
