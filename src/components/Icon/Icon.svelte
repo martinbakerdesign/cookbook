81e8dc9ece4178e264326ce8986636da8f4becbc
Martin Baker <martinbakerdesign@gmail.com>
2022-08-17 11:11:19 +1000

    Switch from paths to <use> references

--- src/components/Icon/Icon.svelte
+++ src/components/Icon/Icon.svelte
@@ -1,14 +1,13 @@
 <script>
-  import iconDefs from "./iconDefs";
   export let icon = "";
   export let label = "";
   export let role = "presentation";
-  export let fill;
-  let viewBox = icon.split("--")[1] ?? 24;
   export let size = 24;
-  let paths = iconDefs[icon];
+  export let style = "";
 
-  let style = `${fill ? `--fill:${fill}` : ""};`;
+  import "./Icon.scss";
+
+  let viewBox = +icon.split("--").pop() ?? 24;
 </script>
 
 <svg
@@ -20,22 +19,6 @@
   aria-label={label}
   {style}
 >
-  {#each paths as d}
-    <path {d} />
-  {/each}
+  {#if !!label.trim().length}<title>{label}</title>{/if}
+  <use href="#icon--{icon}" />
 </svg>
-
-<style lang="scss">
-  @import "../../styles/colours.scss";
-  @import "../../styles/sizes.scss";
-
-  :global(:root) {
-    --fill: #211e20;
-    --hover: #211e20;
-  }
-
-  .icon {
-    flex: none;
-    fill: var(--fill);
-  }
-</style>
