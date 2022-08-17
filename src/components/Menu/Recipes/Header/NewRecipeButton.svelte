2d23da877f2c49f5090f5c5e1c9090f5aabbab49
Martin Baker <martinbakerdesign@gmail.com>
2022-08-17 11:26:32 +1000

    Use menu__header__button class for styling

--- src/components/Menu/Recipes/Header/NewRecipeButton.svelte
+++ src/components/Menu/Recipes/Header/NewRecipeButton.svelte
@@ -21,53 +21,11 @@
   }
 </script>
 
-<button id="header__newrecipe" on:click={newRecipe}
-  ><Icon {...iconProps} /> New Recipe</button
+<button
+  id="header__newrecipe"
+  class="menu__header__button menu__header__button--primary"
+  on:click={newRecipe}><Icon {...iconProps} /> New Recipe</button
 >
 
 <style lang="scss">
-  @use "../../../../styles/colours" as c;
-  @use "../../../../styles/sizes" as s;
-  @use "../../../../styles/typo" as t;
-
-  #header__newrecipe {
-    font-family: inherit;
-    background-color: transparent;
-    outline: 0;
-    border: 0;
-    font-weight: 500;
-    font-size: 0.9375rem;
-    background-color: c.$accent;
-    color: white;
-    letter-spacing: calc(-0.2 / 16 * 1em);
-    line-height: 1.25rem;
-    padding: s.$s2;
-    padding-right: s.$s3;
-    margin: 0;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    gap: 0.25em;
-    border-radius: 0.375rem;
-    --fill: #fff;
-    --hover: #f5853f;
-    transition: 75ms cubic-bezier(0.19, 1, 0.22, 1);
-
-    @mixin focus {
-      background-color: transparent;
-      color: c.$accent;
-      --fill: #f5853f;
-      box-shadow: 0px 0px 0.5rem 0px c.$accent;
-      transition-duration: 150ms;
-    }
-
-    &:focus-visible {
-      @include focus;
-    }
-    @media (hover: hover) {
-      &:hover {
-        @include focus;
-      }
-    }
-  }
 </style>
