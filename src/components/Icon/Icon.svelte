7344784500c719e4688af6169ac11117d980cad5
Martin Baker <martinbakerdesign@gmail.com>
2024-07-21 10:54:32 +1000

    refactor(components/Icon): :recycle: Refactor Icon component
    Separate business logic, set and use defaultStyle variable, only render markup if icon definition is "set"

--- src/components/Icon/Icon.svelte
+++ src/components/Icon/Icon.svelte
@@ -1,15 +1,19 @@
 <script>
+  import {getViewbox, defaultSize} from '.'
+  import "./Icon.scss";
+
   export let icon = "";
   export let label = "";
   export let role = "presentation";
-  export let size = 24;
+  export let size = defaultSize;
   export let style = "";
 
-  import "./Icon.scss";
+  const viewBox = getViewbox(icon, size ?? defaultSize);
 
-  let viewBox = +icon.split("--").pop() ?? 24;
+  const showIcon = icon && icon.length;
 </script>
 
+{#if showIcon}
 <svg
   viewBox={`0 0 ${viewBox} ${viewBox}`}
   width={size}
@@ -22,3 +26,4 @@
   {#if !!label.trim().length}<title>{label}</title>{/if}
   <use href="#icon--{icon}" />
 </svg>
+{/if}
