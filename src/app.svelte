36b0ebfd43dab5f7133b129fe18a45b412612f98
Martin Baker <martinbakerdesign@gmail.com>
2024-07-21 10:37:03 +1000

    refactor(App): :recycle: Separate business logic from App template

--- src/app.svelte
+++ src/app.svelte
@@ -1,51 +1,26 @@
 <script>
-  import { bg } from "./store";
   import user, { checked } from "store/user";
   import Router, { location } from "svelte-spa-router";
-  import useIcons from "components/Icon/useIcons";
+  import {userRoutes, guestRoutes} from 'routes'
+  import _$ from 'utils/dom/querySelector'
+  import {wok} from 'constants/anim'
+  import setBG from "utils/bg/setBG";
+  
+  import {useIcons} from "components/Icon";
+  import Settings from "components/Settings";
+  import Animation from "components/Animation";
 
-  import Signin from "routes/Signin/Signin.svelte";
-  import Menu from "routes/Menu/Menu.svelte";
-  import Recipe from "routes/Recipe/Recipe.svelte";
-  import Settings from "components/Settings/Settings.svelte";
-  import Header from "components/Header/Header.svelte";
-  import Animation from "components/Animation/Animation.svelte";
   import "styles/styles.scss";
-  import { scrollY } from "store/menu";
   import "styles/_colours.scss";
 
   useIcons();
-
-  const userRoutes = {
-    "/:id": Recipe,
-    "/": Menu,
-  };
-  const guestRoutes = {
-    "/:id": Recipe,
-    "/": Signin,
-  };
-
-  let routes = guestRoutes;
-  $: routes = $user ? userRoutes : guestRoutes;
-
-  const animProps = {
-    animation: "loading--wok--64",
-    fps: 30,
-    fill: "#f5853f",
-  };
-
-  $: document.querySelector("html").style.backgroundImage =
-    $location === "/" ? `url(/assets/img/landing/${$bg[0]}.webp)` : "";
+  $: setBG($location);
 </script>
 
-<svelte:window bind:scrollY={$scrollY} />
-{#if $checked && ((!$user && $location !== "/") || $user)}
-  <Header />
-{/if}
 {#if !$checked && $location === "/"}
   <div id="app__loading">
     <div class="spinner">
-      <Animation {...animProps} />
+      <Animation {...wok} />
     </div>
   </div>
 {:else if $checked}
