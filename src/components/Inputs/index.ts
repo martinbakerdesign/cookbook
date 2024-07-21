111324d448d2319c8208f847d502c19efb353fa6
Martin Baker <martinbakerdesign@gmail.com>
2024-07-21 11:47:04 +1000

    refactor(components/Input): :recycle: Create index file for Input component
    Create index file for defining component exports

---
+++ src/components/Inputs/index.ts
@@ -0,0 +1,14 @@
+export {default as AutosizeTextarea} from './AutosizeTextarea'
+export {default as Select} from './Select'
+export {default as Switch} from './Switch'
+
+import AutosizeTextarea from './AutosizeTextarea'
+import Select from './Select'
+import Switch from './Switch'
+const inputs = {
+    AutosizeTextarea,
+    Select,
+    Switch,
+}
+
+export default inputs;
