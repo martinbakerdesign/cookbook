b5fa8a2690935063c03173aacc6ba529433e0369
Martin Baker <martinbakerdesign@gmail.com>
2024-08-01 15:13:11 +1000

    refactor(components/Hotkey): :recycle: Replace hotkey styling with tailwind classes

--- src/components/Hotkey/Hotkey.svelte
+++ src/components/Hotkey/Hotkey.svelte
@@ -2,22 +2,6 @@
   export let hotkey = "";
 </script>
 
-<div class="hotkey">
+<div class="hotkey text-icon-secondary border-icon-secondary py-1 px-2 rounded-md inline-block text-body-sm user-select-none pointer-events-none border rounded-1">
   {hotkey}
 </div>
-
-<style lang="scss">
-  @use "../../styles/sizes" as sizes;
-
-  .hotkey {
-    border: 1px solid var(--text-primary);
-    color: var(--text-primary);
-    padding: sizes.$s1 sizes.$s2;
-    border-radius: 0.5em;
-    display: inline-block;
-    font-size: 0.75rem;
-    user-select: none;
-    opacity: 0.65;
-    pointer-events: none;
-  }
-</style>
