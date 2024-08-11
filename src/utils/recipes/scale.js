e43cec577fb13fcf598bf8415a326cef7caa1bd9
Martin Baker <74693004+martinbakerdesign@users.noreply.github.com>
2024-08-11 14:38:12 +1000

    fix(routes/Recipe): :bug: Fix prosemirror updating on scaleFactor changes (#6)

--- src/utils/recipes/scale.js
+++ src/utils/recipes/scale.js
@@ -6,38 +6,32 @@ let scaled;
 
 const fragmentTypes = [NODES.INGREDIENT, NODES.STEP];
 
+let scale = 1
+
 export default function scaleRecipe(recipe = null, scaleFactor = 1) {
   if (!recipe || !Object.keys(recipe).length || scaleFactor === 1)
     return recipe ?? null;
 
-  let amount = scaleAmount(recipe.amount, +scaleFactor);
+  scale = +scaleFactor;
 
-  let scaledText;
+  const amount = scaleAmount(recipe.amount,scale);
 
   scaled = {
     ...recipe,
-    ingredients: recipe.ingredients.map((i) => {
-      if (!fragmentTypes.includes(i.type)) return i;
-
-      scaledText = new RecipeFragment(i.text, 0).scale(+scaleFactor);
-
-      return {
-        ...i,
-        text: scaledText,
-      };
-    }),
-    method: recipe.method.map((i) => {
-      if (!fragmentTypes.includes(i.type)) return i;
-
-      scaledText = new RecipeFragment(i.text, 0).scale(+scaleFactor);
-
-      return {
-        ...i,
-        text: scaledText,
-      };
-    }),
+    ingredients: recipe.ingredients.map(scaleNode),
+    method: recipe.method.map(scaleNode),
+    mise_en_place: recipe.mise_en_place.map(scaleNode),
     amount,
   };
 
   return scaled;
+}
+
+function scaleNode (node) {
+  if (!fragmentTypes.includes(node.type)) return node;
+
+  return {
+    ...node,
+    text: new RecipeFragment(node.text, 0).scale(scale),
+  };
 }
