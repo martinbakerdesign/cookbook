672cde5c7994bb5aeb7905fc0614cf3f49b960db
Martin Baker <74693004+martinbakerdesign@users.noreply.github.com>
2024-08-20 11:47:52 +1000

    refactor(routes/Recipe): :recycle: Remove business logic from svelte component (#21)

--- src/routes/Recipe/Overview/Recipe__Overview__Yield.svelte
+++ src/routes/Recipe/Overview/Recipe__Overview__Yield.svelte
@@ -2,10 +2,11 @@
   import {AutosizeTextarea} from "components/Inputs";
   import {amount, scaleFactor} from 'store/index'
   import {canEdit} from 'store/recipe'
-  import { derived, get } from "svelte/store";
-  import { findValues } from "utils/recipes/fragment/RecipeFragment";
+  import { derived } from "svelte/store";
+  import {
+    onYieldInput
+  } from '.'
   import scaleAmount from "utils/text/scaleAmount";
-  import unscaleAmount from "utils/text/unscaleAmount";
 
   const id = 'recipe__yield';
 
@@ -13,21 +14,7 @@
     [amount, scaleFactor],
     ([$amount, $scaleFactor]) => scaleAmount($amount, $scaleFactor)
   );
-  function onInput(e) {
-    const unscaled = unscaleAmount(e.target.value, get(scaleFactor));
-    
-    if (unscaled === get(amount)) return;
-
-    const value = findValues(unscaled)[0];
-    const quantity = value?.value ?? null;
-    const unit = (value.value ? unscaled.slice(value.pos+value.quantitySize+1) : '').trim();
-
-    amount.set({
-      quantity,
-      unit,
-      text: unscaled
-    });
-  }
+  
 </script>
 
 <section data-section="YIELD" class="flex-1 border-r flex flex-col">
@@ -40,8 +27,8 @@
       readonly: !$canEdit
     }}
     value={$inputValue}
-    on:input={onInput}
-    on:change={onInput}
+    on:input={onYieldInput}
+    on:change={onYieldInput}
     class="text-heading-lg font-regular placeholder:text-text-secondary bg-transparent pl-page pr-10 space-y-10 pt-[3.125rem] pb-12 outline-none"
   />
 </section>
