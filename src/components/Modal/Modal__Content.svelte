efccc136b87d42096c70f4bbfeebf4239490ac34
Martin Baker <martinbakerdesign@gmail.com>
2024-11-30 14:25:35 +1100

    fix(routes/Menu): :bug: Attempt to fix modal responsiveness on mobile

--- src/components/Modal/Modal__Content.svelte
+++ src/components/Modal/Modal__Content.svelte
@@ -1,7 +1,13 @@
 <script></script>
 
-<div class="rounded-2 backdrop-blur-md bg-background-surface shadow-xl w-full max-w-[94vw] lg:max-w-[36rem] flex flex-col gap-y-16 p-3">
+<div class="rounded-2 backdrop-blur-md bg-background-surface shadow-xl w-full max-w-[94vw] lg:max-w-[36rem] flex flex-col gap-y-16 p-3 modal-content-container">
     <slot name="header"/>
     <slot/>
     <slot name="footer"/>
 </div>
+
+<style>
+    .modal-content-container {
+        max-height: calc(100% - 0.75rem);
+    }
+</style>
